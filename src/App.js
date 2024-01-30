3ef45c53e7763c07e87b4619e632565225297234
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-01-30 21:34:25 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+function NotFoundPage() {
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(() => history.push('/'), 5000);
+  });
+  return <div>404 Not Found</div>;
 }
 
 export default App;
